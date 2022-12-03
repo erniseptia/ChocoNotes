6ae47b412deee2507e1d32a4045e7e65d168e2b6
erniseptia <92901309+erniseptia@users.noreply.github.com>
2022-12-03 09:27:26 +0700

    xx

---
+++ app/src/main/java/com/erni/choconotes/FormNoteActivity.kt
@@ -0,0 +1,75 @@
+package com.erni.choconotes
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_form_note.*
+import java.util.HashMap
+
+
+
+class FormNoteActivity : AppCompatActivity(){
+    var note : Note? = null
+
+    private val java: Class<*>?
+        get() {return null}
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_note)
+
+        val data = intent.getSerializableExtra("note")
+        var edit = true
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("notes")
+
+        if (data != null) {
+            note = data as Note
+            etNoteNameEdit.setText(note?.title)
+            etNoteDescriptionEdit.setText(note?.description)
+
+            btActForm.setText("Edit")
+        } else {
+            btActForm.setText("Tambah")
+            edit = false
+        }
+
+        btActForm.setOnClickListener {
+            if (edit) {
+                val changeData = HashMap<String, Any>()
+                changeData.put("title", etNoteNameEdit.text.toString())
+                changeData.put("description", etNoteDescriptionEdit.text.toString())
+
+                myRef.child(note?.key.toString()).updateChildren(changeData)
+                finish()
+                startActivity(Intent(this, java))
+            } else {
+                val key = myRef.push().key
+
+                val newNote = Note()
+                newNote.title = etNoteNameEdit.text.toString()
+                newNote.description = etNoteDescriptionEdit.text.toString()
+
+                myRef.child(key.toString()).setValue(newNote)
+                finish()
+                startActivity(Intent(this, java))
+            }
+        }
+    }
+
+    fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+class TODO(s: String) {
+
+}
+
---
+++ app/src/main/java/com/erni/choconotes/Note.kt
@@ -0,0 +1,9 @@
+package com.erni.choconotes
+
+import java.io.Serializable
+
+class Note : Serializable {
+    var key : String? = null
+    var title : String? = null
+    var description : String? = null
+}
